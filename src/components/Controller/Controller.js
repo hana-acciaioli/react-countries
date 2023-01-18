2d3b87f39af7ab337a7c72e98ceeaff389a4c399
Hana Acciaioli <hana.acciaioli@gmail.com>
2023-01-18 15:00:13 -0800

    continent filter working

---
+++ src/components/Controller/Controller.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Controller(option) {
+  return (
+    <controller>
+      <select>
+        <option value={option}>option</option>
+      </select>
+    </controller>
+  );
+}
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import { useState } from 'react';
 import './Main.css';
 import CountryCard from '../CountryCard/CountryCard';
 import { useCountries } from '../../hooks/useCountries';
+// import Controller from '../Controller/Controller';
 
 export default function Main() {
   const countries = useCountries();
+  const [continent, setContinent] = useState('all');
+  const continents = [...new Set(countries.map(({ continent }) => continent))];
+  const filtered = countries.filter(
+    (country) => country.continent === continent || continent === 'all'
+  );
   return (
     <main className="main">
-      {countries.map((country) => (
+      <select onChange={(e) => setContinent(e.target.value)}>
+        <option value="all">all</option>
+        {continents.map((continent) => (
+          <option key={continent} value={continent}>
+            {continent}
+          </option>
+        ))}
+      </select>
+      {filtered.map((country) => (
         <CountryCard key={country.id} {...country} />
       ))}
     </main>
